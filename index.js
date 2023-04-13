9d9f538c9ab0eba387059c7c09f37df7e23f9178
changach-3000 <sharon.changach@student.moringaschool.com>
2023-04-13 10:40:29 +0300

    Added categories

--- index.js
+++ index.js
@@ -1,14 +1,79 @@
 const container = document.getElementById("container");
+// Add Buttons
+const catbut = document.getElementById("explicit")
+const devbut = document.getElementById("dev")
+const fbut = document.getElementById("fashion")
+const histbut = document.getElementById("history")
+const relbut = document.getElementById("religion")
 
-fetch("https://api.chucknorris.io/jokes/random")
+// fetch dev jokes
+fetch("https://api.chucknorris.io/jokes/random?category=dev")
 .then((response) => response.json())
-.then((jokes=>randomJokes(jokes)))
+.then((jokes=>devJokes(jokes)))
 
-function randomJokes(jokes){
+function devJokes(jokes){
     //create a variable to render the jokes to
-    const value = document.createElement("p");
-    value.id = "random"
-    value.innerHTML = jokes.value;
-    container.appendChild(value);
+    devbut.addEventListener("click", (event) =>{
+        //create a variable to render the jokes to
+        const value = document.getElementById("random1")
+        value.innerHTML = jokes.value;
+    })
+}
 
+// Fetch explicit jokes
+fetch("https://api.chucknorris.io/jokes/random?category=explicit")
+.then((response) => response.json())
+.then((jokes =>explicitJokes(jokes)))
+
+function explicitJokes(jokes){
+    catbut.addEventListener("click", (event) =>{
+        event.preventDefault();
+        //create a variable to render the jokes to
+        const value1 = document.getElementById("random1")
+        value1.innerHTML = jokes.value;
+    })
+}
+
+// Fetch history jokes
+fetch("https://api.chucknorris.io/jokes/random?category=history")
+.then((response) => response.json())
+.then((jokes=>historyJokes(jokes)))
+
+function historyJokes(jokes){
+    //create a variable to render the jokes to
+    histbut.addEventListener("click", (event) =>{
+        //create a variable to render the jokes to
+        const value = document.getElementById("random1")
+        value.innerHTML = jokes.value;
+    })
+    
+}
+
+// fetch fashion jokes
+fetch("https://api.chucknorris.io/jokes/random?category=fashion")
+.then((response) => response.json())
+.then((jokes=>fashionJokes(jokes)))
+
+function fashionJokes(jokes){
+    //create a variable to render the jokes to
+    histbut.addEventListener("click", (event) =>{
+        //create a variable to render the jokes to
+        const value = document.getElementById("random1")
+        value.innerHTML = jokes.value;
+    })
+    
+}
+// Fetch religious jokes
+fetch("https://api.chucknorris.io/jokes/random?category=religion")
+.then((response) => response.json())
+.then((jokes=>religionJokes(jokes)))
+
+function religionJokes(jokes){
+    //create a variable to render the jokes to
+    relbut.addEventListener("click", (event) =>{
+        //create a variable to render the jokes to
+        const value = document.getElementById("random1")
+        value.innerHTML = jokes.value;
+    })
+    
 }
